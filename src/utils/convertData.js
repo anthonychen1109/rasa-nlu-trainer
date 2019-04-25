f32af71557f56ef51d918de6496a1ac4c5bb818b
anthonychen1109 <chen.anthony.1109@gmail.com>
2019-04-25 17:27:25 -0400

    added convert data

--- src/utils/convertData.js
+++ src/utils/convertData.js
@@ -1,11 +1,11 @@
+import MarkDownIt from 'markdown-it'
+import WriteFileP from "write-file-p";
 import nluData from '../state/nlu_data.md';
 import testData from '../state/testData.json';
 
 export default function () {
     testData.rasa_nlu_data.common_examples.map(e => {
-        console.log('nluData', nluData)
         let {text, intent, entities} = e
-        // let [entities] = e
         let createIntent = "## intent:" + intent
         console.log(createIntent)
         if (entities.length > 0) {
@@ -13,7 +13,10 @@ export default function () {
                 let keyword = `[${text.slice(e.start, e.end)}]`
                 let entity = `(${e.entity})`
                 let replaceWord = "` " + text.replace(e.value, keyword) + entity
-                console.log('final string', replaceWord)
+                let md = new MarkDownIt()
+                WriteFileP(nluData, md.render(replaceWord), (err, data) => {
+                    console.log(err || data);
+                });
             })
         }
     })
